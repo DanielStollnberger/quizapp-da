a86d5f869e0f0601c024ac98a043e232e1be82e3
Daniel Stollnberger <daniiistolliii@gmail.com>
2025-03-29 22:34:30 +0100

    added some shit

---
+++ script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "question":"Who was the first president of the united states of america?",
+        "answer-1":"Mark Gebauer",
+        "answer-2":"Hakan Calagnoglu",
+        "answer-3":"Timmothy Weah",
+        "answer-4":"George Washington",
+        "right-answer":"George Washington"
+    },
+    {
+        "question":"Wer hat die Mutter von Jeremy in den A.... gef....?",
+        "answer-1":"Major Monogwam der geile oida",
+        "answer-2":"Ferb",
+        "answer-3":"Jeremy",
+        "answer-4":"Perry",
+        "right-answer":"Major Monogwam der geile oida"
+    },
+    {
+        "question":"How many questionmarks does the 'Three Questionmarks' have?",
+        "answer-1":"Thirty-eight",
+        "answer-2":"Seven",
+        "answer-3":"Three",
+        "answer-4":"Twenty-one",
+        "right-answer":"Three"
+    },
+    {
+        "question":"Who ate the chicken-shawarma?",
+        "answer-1":"Billy",
+        "answer-2":"Marcus",
+        "answer-3":"Thomas",
+        "answer-4":"Abdulrahman Saleimak Vahid Mohammed",
+        "right-answer":"Abdulrahman Saleimak Vahid Mohammed"
+    },
+    {
+        "question":"Who's the real G.O.A.T?",
+        "answer-1":"Christiano Ronaldo",
+        "answer-2":"Lionel Messi",
+        "answer-3":"Anthony",
+        "answer-4":"IShowSpeed",
+        "right-answer":"Anthony"
+    }
+]
